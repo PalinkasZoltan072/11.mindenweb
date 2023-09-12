9a06026529eb3e03b7701f6bcaf20689245e2d4f
Palinkas.Zoltan <palinkas.zoltan@ckik.hugit config --global --listgit config --global user.email palinkas.zoltan@ckik.hu>
2023-09-12 13:01:17 +0200

    sulis cucc

---
+++ c#/2023.09.05_Alapok/2023.09.05_Alapok/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023._09._05_Alapok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Egy soros komment
+            /*
+             több soros komment
+             */
+
+            Console.Write("Első sor. \n"); //n sortores karakterjelzese
+            // \t tabulátor használata
+            Console.WriteLine(); // sortores
+
+            Console.WriteLine("Második sor."); // kiir sortores
+
+            // Console.ReadLine(); //enter leutesere var
+            Console.ReadKey();// barmely billentyu leutesere var (ctrl k c-u komment tobbsoros)
+        }
+    }
+}
