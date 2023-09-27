b262627f1f59ab29b528ef79fda42e1ffd81a0f0
PALINKAS.ZOLTAN <palinkas.zoltan@ckik.hu>
2023-09-27 19:19:29 +0200

    14-17 pdf

--- asztali alkalmazas/Szovegkezeles/Szovegkezeles/Program.cs
+++ asztali alkalmazas/Szovegkezeles/Szovegkezeles/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -41,6 +43,70 @@ namespace Szovegkezeles
             Console.WriteLine(szoveg.Length);
             Console.WriteLine();
             Console.WriteLine(szoveg[szoveg.Length-1]);
+            Console.WriteLine();
+
+            Console.Write("Adjon meg egy darab betut ");
+            string m = Console.ReadLine();
+            char ű = Convert.ToChar(m);
+
+
+            for (int i = 1;i<szoveg.Length;i += 2)
+            {
+                
+                if (szoveg[i] == ű)
+                {
+                    Console.WriteLine("VAN");
+                }
+                else
+                {
+                    Console.WriteLine("nincs");
+                }
+                
+            }
+            Console.WriteLine();
+            
+
+
+            //14 
+            Console.Write("Adjon meg egy betut ");
+            string v = Console.ReadLine();
+            Console.WriteLine(v[0]);
+            Console.WriteLine();
+            Console.WriteLine(v[v.Length-1]);
+            Console.WriteLine() ;
+            // 15
+            if (v[0] == v[v.Length - 1])
+            {
+                Console.WriteLine("IGEN");
+            }
+            else {
+                Console.WriteLine("NEM");
+            }
+            
+
+            Console.WriteLine();
+            // 16
+            for (int i = 1; i < v.Length; i += 2)
+            {
+
+                Console.Write( v[i]);
+
+            }
+            Console.WriteLine();
+            // 17
+            Console.WriteLine();
+
+            for (int i =(v.Length-1); i >= 0; i -= 1)
+            {
+                
+                Console.WriteLine(v[i]);
+
+            }
+            Console.WriteLine();
+
+
+
+
 
             Console.ReadKey(); 
         }
